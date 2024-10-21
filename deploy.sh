e6e50564b25741eb7f52e6cf0ff8aba7f2681a5c
bebok <bebok>
2024-10-21 19:37:04 +0200

    change

--- deploy.sh
+++ deploy.sh
@@ -5,3 +5,5 @@ rm -rf yt_helper_deploy
 git clone yt_helper yt_helper_deploy
 rm -rf yt_helper_deploy/.git
 scp -i ~/.ssh/zonki-aws.pem -r yt_helper_deploy ec2-user@44.206.246.12:~/app
+
+super ekstra zmiana
